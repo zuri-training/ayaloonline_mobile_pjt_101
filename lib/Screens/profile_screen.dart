acb6b9954c18ffc17dfa91faad47b42c155161f0
Odinachi <odinachidavid@yahoo.com>
2021-06-20 09:38:29 +0100

    cleaned up the code

--- lib/Screens/profile_screen.dart
+++ lib/Screens/profile_screen.dart
@@ -1,19 +1,5 @@
-import 'package:ayalo_mobile_pjt101/Custom_widgets/bottom_navbar.dart';
 import 'package:ayalo_mobile_pjt101/constants/colors.dart';
 import 'package:flutter/material.dart';
-
-// class ProfileScreen extends StatelessWidget {
-//   static String id = 'ProfileScreen';
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       bottomNavigationBar: CustomBottomBar(
-//         pageId: id,
-//       ),
-//       body: ProfileDetailsScreen(),
-//     );
-//   }
-// }
 
 class ProfileDetailsScreen extends StatelessWidget {
   @override
