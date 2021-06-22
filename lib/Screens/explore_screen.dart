cc011ad30ee25da430e18c4e4ee4cd6009e4f12f
Odinachi <odinachidavid@yahoo.com>
2021-06-22 21:02:00 +0100

    k

--- lib/Screens/explore_screen.dart
+++ lib/Screens/explore_screen.dart
@@ -2,19 +2,6 @@ import 'package:ayalo_mobile_pjt101/Custom_widgets/bottom_navbar.dart';
 import 'package:ayalo_mobile_pjt101/constants/colors.dart';
 import 'package:flutter/material.dart';
 
-// class ExploreScreen extends StatelessWidget {
-//   static String id = "ExploreScreen";
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       bottomNavigationBar: CustomBottomBar(
-//         pageId: id,
-//       ),
-//       body: ExploreListScreen(),
-//     );
-//   }
-// }
-
 class ExploreListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
